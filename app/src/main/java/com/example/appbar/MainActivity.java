1beb9ff7f5cd5074a9d2bad34d878806182a39bc
serg0 <serg.01@bk.ru>
2021-03-16 04:53:28 +0300

    fix code style

--- app/src/main/java/com/example/appbar/MainActivity.java
+++ app/src/main/java/com/example/appbar/MainActivity.java
@@ -10,20 +10,19 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar myToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar =(Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
 
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
 
@@ -34,24 +33,24 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_open_notes) {
-            Toast.makeText(MainActivity.this, "Отркыть записную книжку", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, R.string.open_notes, Toast.LENGTH_LONG).show();
             Intent intentNotes = new Intent(MainActivity.this, NotesActivity.class);
             startActivity(intentNotes);
             return true;
         }
         if (id == R.id.action_price) {
-            Toast.makeText(MainActivity.this, "Отркыть список цен", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, R.string.open_price, Toast.LENGTH_LONG).show();
             Intent intentNotes = new Intent(MainActivity.this, NotesActivity.class);
             startActivity(intentNotes);
             return true;
         }
         if (id == R.id.action_products) {
-            Toast.makeText(MainActivity.this, "Отркыть список товаров", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, R.string.open_products, Toast.LENGTH_LONG).show();
             Intent intentNotes = new Intent(MainActivity.this, PriceActivity.class);
             startActivity(intentNotes);
             return true;
         }if (id == R.id.action_settings) {
-            Toast.makeText(MainActivity.this, "Отркыть записную найстроки ", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, R.string.open_settings, Toast.LENGTH_LONG).show();
             Intent intentNotes = new Intent(MainActivity.this, NotesActivity.class);
             startActivity(intentNotes);
             return true;
